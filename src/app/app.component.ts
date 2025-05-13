9092049fdbb1a8fb2d6f6f64d16144fd68e867df
Bryan Romero <bryan.romerodev@gmail.com>
2025-05-13 12:32:41 +0200

    Init: scoreboard UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ScoreboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/scoreboard/scoreboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-scoreboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './scoreboard.component.html',
+  styleUrl: './scoreboard.component.scss'
+})
+export class ScoreboardComponent {
+
+}
